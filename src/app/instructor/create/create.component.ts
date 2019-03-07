9aead4480d3f17463dbd4d829c22edeb123886b6
Surjeet Singh <bisht.surjeetsingh@gmail.com>
2019-03-07 16:54:50 +0530

    sasasas

--- src/app/instructor/create/create.component.ts
+++ src/app/instructor/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { InstructorService } from '../instructor.service';
 
 @Component({
@@ -9,13 +10,13 @@ import { InstructorService } from '../instructor.service';
 export class InstructorCreateComponent implements OnInit {
 
   public model: any = {};
-  constructor(private _instructorService: InstructorService) { }
+  constructor(private _instructorService: InstructorService, private _router: Router) { }
 
   ngOnInit() {
   }
   createEmployee() {
     this._instructorService.registerEmployee(this.model).subscribe(res => {
-      alert(res);
+      this._router.navigate(['/admin/instructor/list']);
     })
   }
 }
